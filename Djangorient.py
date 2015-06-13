5ff10413a2aad678e50353f5d6a228af55eea665
tom <tomtalp93@gmail.com>
2015-06-13 12:29:08 +0300

    Basic changes, not using Rexster anymore

--- Djangorient.py
+++ Djangorient.py
@@ -1,61 +1,39 @@
 from django.conf import settings
-from bulbs.rexster import RexsterClient
-from bulbs.config import Config
-from DjangorientQueryManager import DjangorientQueryManager
+from djangorient.DjangorientHttpClient import HttpClient
 
-class DjangorientConnection(object):
+class DjangorientClient(object):
 	def __init__(self):
+		self._username = settings.DJANGORIENT_SETTINGS['username']
+		self._password = settings.DJANGORIENT_SETTINGS['password']
+		self._db_name = settings.DJANGORIENT_SETTINGS['name']
+		self._host = settings.DJANGORIENT_SETTINGS['host']
+		self._port = settings.DJANGORIENT_SETTINGS['port']
+		self._base_uri = 'http://{host}:{port}'.format(host = self._host, port = self._port)
+		self._http_client = HttpClient(
+							self._base_uri, 
+							self._db_name
+							self._username, 
+							self._password)
 
-		self.connection = None
-		self._rexster_config = None
-		self.QueryManager = None
+	def disconnect(self):
+		disconn_uri = self._base_uri + '/disconnect/'
+		self._http_client.send_request(disconn_uri, 'GET')
 
-	def _create_bulb_config(self):
-		"""
-		Build a bulbs Rexster configuration object via settings.py data
-		"""
-		rexster_graph_uri = settings.DJANGORIENT_SETTINGS['REXSTER_DB_URI']
-		bulbs_rexster_config = Config(rexster_graph_uri)
+	## TODO - Implement a proper test
+	def test_connection(self):
+		conn_uri = self._base_uri + '/connect/' + self._db_name
+		return self._http_client.send_request(conn_uri, 'GET')
 
-		return bulbs_rexster_config
-
-	def setup(self):
-		self._rexster_config = self._create_bulb_config()
-		self.connect()
-
-	def connect(self):
-		try:
-			self._rexster_config = self._create_bulb_config()		
-			self.connection = RexsterClient(self._rexster_config)
-		except Exception, e:
-			raise e
-
-	def close(self):
-		pass
-
-	def query_manager(self):
-		"""
-		Return the query manager object
-		"""
-		self.QueryManager = DjangorientQueryManager(self.connection)
-		return self.QueryManager
-
-	def create_query_manager(self):
-		"""
-		Initialize the Djangorient QueryManager
-		"""
-		self.QueryManager = DjangorientQueryManager(self.connection)
+	def test_GET(self):
+		uri = self._base_uri + '/document/' + self._db_name + '/1:0'
+		r1 = self._http_client.send_request(uri, 'GET')
+		return r1
 
 
-	def test_query(self):
-		"""
-		TEST
-		"""
-		print self.connection.gremlin("g.V.count()").results.next().data
+	
+c1 = DjangorientClient()
+c1.test_connection()
+#print c1.test_GET()
 
 
-c = DjangorientConnection()
-c.connect()
-qm = c.query_manager()
-results = qm.gremlin("g.V.count()")
-print results.results_iter.next().data
+#h1 = c1._http_client
---
+++ DjangorientHttpClient.py
@@ -0,0 +1,73 @@
+import httplib2
+import json
+
+class HttpClient(object):
+	def __init__(self, base_uri, db_name, username, password):
+		self._http = httplib2.Http()
+		self._http.add_credentials(username, password)
+		self._request_headers = self._get_headers()
+
+		self._base_uri = base_uri
+		self._db_name = db_name
+
+	def _get_headers(self):
+		"""
+		Headers for HTTP request
+		"""
+		return {'Content-Type': 'application/json; charset=UTF-8',
+				'Accept-Encoding': 'gzip,deflate'}
+
+	def send_request(self, uri, method, data=None):
+		if type(data) is str:
+			try:
+				json.loads(data)
+			except ValueError, e:
+				raise Exception("The JSON format of your data is invalid")
+
+		elif type(data) is dict:
+			data = json.dumps(data)
+
+		elif data is not None:
+			raise Exception("Data must a dictionary or raw JSON")
+
+		## TODO - Add more methods
+		if method.upper() == 'POST':
+			return self._POST_request(uri, data)
+		else:
+			return self._GET_request(uri)
+
+	def _POST_request(self, uri, data):
+		headers = self._request_headers
+		headers['Content-Length'] = str(len(data))
+
+		resp, content = self._http.request(
+			uri = uri,
+			method = 'POST',
+			headers = headers,
+			body = data
+		)
+		return DjangorientResults(resp, content)
+
+	def _GET_request(self, uri):
+		resp, content = self._http.request(
+			uri = uri,
+			method = 'GET',
+			headers = self._request_headers,
+		)
+
+		return DjangorientResults(resp, content)
+
+class DjangorientResults(object):
+	def __init__(self, resp, content):
+		self._resp = resp
+		self._content = content
+
+		self._check_resp()
+
+	def __str__(self):
+		return self._content
+
+	def _check_resp(self):
+		if self._resp['status'] == '401':
+			raise Exception('Not authorized! Please enter a valid username & pw')
+
